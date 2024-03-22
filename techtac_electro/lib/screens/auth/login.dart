78007620062e53cc56ccfa4ab2c6a2ba6e6c18bc
Mohammad Hirbawi <40454410+mohammadhirbawi@users.noreply.github.com>
2024-03-22 02:35:05 +0200

    Implement the Sign in with google button widget

--- techtac_electro/lib/screens/auth/login.dart
+++ techtac_electro/lib/screens/auth/login.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:techtac_electro/consts/my_validators.dart';
 import 'package:techtac_electro/widgets/app_name_text.dart';
+import 'package:techtac_electro/widgets/auth/google_btn.dart';
 import 'package:techtac_electro/widgets/subtitle_text.dart';
 import 'package:techtac_electro/widgets/text_widget.dart';
 
@@ -172,28 +173,52 @@ class _LoginScreenState extends State<LoginScreen> {
                       const SizedBox(
                         height: 16,
                       ),
-                      Row(
-                        children: [
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              padding: const EdgeInsets.all(12),
-                              backgroundColor:
-                                  Theme.of(context).colorScheme.background,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(
-                                  10,
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SizedBox(
+                          height: kBottomNavigationBarHeight + 10,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              const Expanded(
+                                child: SizedBox(
+                                  height: kBottomNavigationBarHeight,
+                                  child: FittedBox(
+                                    child: GoogleButton(),
+                                  ),
                                 ),
                               ),
-                            ),
-                            child: const Text(
-                              "Guest",
-                              style: TextStyle(fontSize: 20),
-                            ),
-                            onPressed: () async {
-                              _loginfct();
-                            },
+                              const SizedBox(
+                                width: 8,
+                              ),
+                              Expanded(
+                                child: SizedBox(
+                                  height: kBottomNavigationBarHeight,
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      padding: const EdgeInsets.all(12),
+                                      backgroundColor: Theme.of(context)
+                                          .colorScheme
+                                          .background,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(
+                                          10,
+                                        ),
+                                      ),
+                                    ),
+                                    child: const Text(
+                                      "Guest",
+                                      style: TextStyle(fontSize: 20),
+                                    ),
+                                    onPressed: () async {
+                                      _loginfct();
+                                    },
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
-                        ],
+                        ),
                       ),
                       const SizedBox(
                         height: 16,

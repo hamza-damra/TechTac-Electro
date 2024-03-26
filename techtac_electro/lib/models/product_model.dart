1c160bc035e7611f7801bee80f891ffc5ee47e56
Mohammad Hirbawi <40454410+mohammadhirbawi@users.noreply.github.com>
2024-03-26 02:04:27 +0200

    Create the Product Model

---
+++ techtac_electro/lib/models/product_model.dart
@@ -0,0 +1,17 @@
+class ProductModel {
+  final String productId,
+      productTitle,
+      productPrice,
+      productCategory,
+      productImage,
+      productQuantity;
+
+  ProductModel({
+    required this.productId,
+    required this.productTitle,
+    required this.productPrice,
+    required this.productCategory,
+    required this.productImage,
+    required this.productQuantity,
+  });
+}

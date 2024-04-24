5cca29d843da1a1d86cbc36870219fe5068dd77b
Mohammad Hirbawi <40454410+mohammadhirbawi@users.noreply.github.com>
2024-04-25 02:53:04 +0300

    Save the user information to Firestore on Google Sign In

--- techtac_electro/lib/widgets/auth/google_btn.dart
+++ techtac_electro/lib/widgets/auth/google_btn.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -21,6 +22,21 @@ class GoogleButton extends StatelessWidget {
             accessToken: googleAuth.accessToken,
             idToken: googleAuth.idToken,
           ));
+
+          if (authResults.additionalUserInfo!.isNewUser) {
+            await FirebaseFirestore.instance
+                .collection("users")
+                .doc(authResults.user!.uid)
+                .set({
+              "userId": authResults.user!.uid,
+              "userName": authResults.user!.displayName,
+              "userImage": authResults.user!.photoURL,
+              "userEmail": authResults.user!.email,
+              "createdAt": Timestamp.now(),
+              "userWish": [],
+              "userCart": [],
+            });
+          }
           WidgetsBinding.instance.addPostFrameCallback((_) async {
             Navigator.pushReplacementNamed(context, RootScreen.routName);
           });

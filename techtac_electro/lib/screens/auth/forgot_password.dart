c847bbe1f81ca163ff47bce1e024f9d8f8d4778f
Hamza Damra <hamza.damra@students.alquds.edu>
2024-05-23 14:19:49 +0300

    add reset your password logic

--- techtac_electro/lib/screens/auth/forgot_password.dart
+++ techtac_electro/lib/screens/auth/forgot_password.dart
@@ -1,10 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:techtac_electro/consts/my_validators.dart';
 import 'package:techtac_electro/services/assets_manager.dart';
 import 'package:techtac_electro/widgets/app_name_text.dart';
 import 'package:techtac_electro/widgets/subtitle_text.dart';
-import 'package:techtac_electro/widgets/text_widget.dart';
+
+import '../../widgets/text_widget.dart';
 
 class ForgotPasswordScreen extends StatefulWidget {
   static const routeName = '/ForgotPasswordScreen';
@@ -16,7 +18,7 @@ class ForgotPasswordScreen extends StatefulWidget {
 
 class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
   late final TextEditingController _emailController;
-  late final _formKey = GlobalKey<FormState>();
+  late final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -35,7 +37,24 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
   Future<void> _forgetPassFCT() async {
     final isValid = _formKey.currentState!.validate();
     FocusScope.of(context).unfocus();
-    if (isValid) {}
+    if (isValid) {
+      final String email = _emailController.text.trim();
+
+      try {
+        await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Password reset email sent! Check your email.'),
+          ),
+        );
+      } catch (error) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Error: ${error.toString()}'),
+          ),
+        );
+      }
+    }
   }
 
   @override
@@ -54,11 +73,9 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
         },
         child: SafeArea(
           child: ListView(
-            // shrinkWrap: true,
             padding: const EdgeInsets.symmetric(horizontal: 24),
             physics: const BouncingScrollPhysics(),
             children: [
-              // Section 1 - Header
               const SizedBox(
                 height: 10,
               ),
@@ -76,13 +93,12 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
               ),
               const SubtitleTextWidget(
                 label:
-                    'Please enter the email address you\'d like your password reset information sent to',
+                'Please enter the email address you\'d like your password reset information sent to',
                 fontSize: 14,
               ),
               const SizedBox(
                 height: 40,
               ),
-
               Form(
                 key: _formKey,
                 child: Column(
@@ -102,29 +118,21 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
                       validator: (value) {
                         return MyValidators.emailValidator(value);
                       },
-                      onFieldSubmitted: (_) {
-                        // Move focus to the next field when the "next" button is pressed
-                      },
                     ),
                     const SizedBox(height: 16),
                   ],
                 ),
               ),
-
               const SizedBox(
                 height: 20,
               ),
-
               SizedBox(
                 width: double.infinity,
                 child: ElevatedButton.icon(
                   style: ElevatedButton.styleFrom(
                     padding: const EdgeInsets.all(12),
-                    // backgroundColor: Colors.red,
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(
-                        10,
-                      ),
+                      borderRadius: BorderRadius.circular(10),
                     ),
                   ),
                   icon: const Icon(IconlyBold.send),
@@ -135,7 +143,7 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
                     ),
                   ),
                   onPressed: () async {
-                    _forgetPassFCT();
+                    await _forgetPassFCT();
                   },
                 ),
               ),

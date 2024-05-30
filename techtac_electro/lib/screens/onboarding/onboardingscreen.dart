4854d4ac0fa82e48a157a32b8d760b2df1fbed01
hamza <hamza.damra@students.alquds.edu>
2024-05-30 15:33:11 +0300

    fix some issues

--- techtac_electro/lib/screens/onboarding/onboardingscreen.dart
+++ techtac_electro/lib/screens/onboarding/onboardingscreen.dart
@@ -6,6 +6,7 @@ import 'package:techtac_electro/screens/onboarding/onboardingInfo.dart';
 import 'package:techtac_electro/screens/root_screen.dart';
 import 'package:techtac_electro/widgets/subtitle_text.dart';
 import 'package:techtac_electro/widgets/text_widget.dart';
+import 'package:techtac_electro/models/onboarding_model.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
@@ -18,6 +19,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   final controller = OnboardingItems();
   final pageCotroller = PageController();
   bool isLastPage = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,72 +29,71 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
         child: isLastPage
             ? getStarted()
             : Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      pageCotroller.jumpToPage(controller.items.length - 1);
-                    },
-                    child: const Text('Skip'),
-                  ),
-
-                  //indicator
-                  SmoothPageIndicator(
-                    controller: pageCotroller,
-                    count: controller.items.length,
-                    onDotClicked: (index) => pageCotroller.animateToPage(
-                      index,
-                      duration: const Duration(microseconds: 600),
-                      curve: Curves.easeIn,
-                    ),
-                    effect: const WormEffect(
-                      dotHeight: 12,
-                      dotWidth: 12,
-                      activeDotColor: Color(0xFF7357a4),
-                    ),
-                  ),
-
-                  TextButton(
-                    onPressed: () {
-                      pageCotroller.nextPage(
-                          duration: const Duration(milliseconds: 600),
-                          curve: Curves.easeIn);
-                    },
-                    child: const Text('Next'),
-                  ),
-                ],
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            TextButton(
+              onPressed: () {
+                pageCotroller.jumpToPage(controller.items.length - 1);
+              },
+              child: const Text('Skip'),
+            ),
+            // Indicator
+            SmoothPageIndicator(
+              controller: pageCotroller,
+              count: controller.items.length,
+              onDotClicked: (index) => pageCotroller.animateToPage(
+                index,
+                duration: const Duration(microseconds: 600),
+                curve: Curves.easeIn,
               ),
+              effect: const WormEffect(
+                dotHeight: 12,
+                dotWidth: 12,
+                activeDotColor: Color(0xFF7357a4),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                pageCotroller.nextPage(
+                    duration: const Duration(milliseconds: 600),
+                    curve: Curves.easeIn);
+              },
+              child: const Text('Next'),
+            ),
+          ],
+        ),
       ),
       body: Container(
         margin: const EdgeInsets.symmetric(horizontal: 15),
         child: PageView.builder(
-            onPageChanged: (index) => setState(
-                () => isLastPage = controller.items.length - 1 == index),
-            itemCount: controller.items.length,
-            controller: pageCotroller,
-            itemBuilder: (context, index) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(controller.items[index].image),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  TitlesTextWidget(
-                    label: controller.items[index].title,
-                    fontSize: 30,
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  SubtitleTextWidget(
-                    label: controller.items[index].description,
-                    textAlign: TextAlign.center,
-                    color: Colors.grey,
-                  ),
-                ],
-              );
-            }),
+          onPageChanged: (index) => setState(
+                  () => isLastPage = controller.items.length - 1 == index),
+          itemCount: controller.items.length,
+          controller: pageCotroller,
+          itemBuilder: (context, index) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(controller.items[index].image),
+                const SizedBox(
+                  height: 15,
+                ),
+                TitlesTextWidget(
+                  label: controller.items[index].title,
+                  fontSize: 30,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                SubtitleTextWidget(
+                  label: controller.items[index].description,
+                  textAlign: TextAlign.center,
+                  color: Colors.grey,
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
